e3a0ec8376a42b73684cae0114c2ede08478d12b
karan <karan.ker@gmail.com>
2015-07-18 19:33:33 -0400

    added basic authentication

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 var Hapi = require('hapi'),
-	Bell = require('bell');
+	Bell = require('bell'),
+    RequestModule = require('request');
+
+var clientId = "57acad3f5f1aa298c29d";
+var clientSecret = process.env.CLIENT_SECRET;
 
 var server = new Hapi.Server();
 server.connection({ port: 9000 });
@@ -55,8 +59,35 @@ server.register(Bell, function (err) {
 	    }
 	});
 
-    server.start(function (err) {
+    server.route({
+        method: 'GET',
+        path: '/github/authorize',
+        handler: function (request, reply) {
+            var singleUseCode = request.query.singleUseCode;
+            console.log(clientSecret);
+            RequestModule({
+                url: 'https://github.com/login/oauth/access_token', //URL to hit
+                method: 'POST',
+                //Lets post the following key/values as form
+                json: {
+                    client_id: clientId,
+                    client_secret: clientSecret,
+                    code: singleUseCode,
+                    async: false
+                }
+                }, function(error, response, body){
+                    if(error) {
+                        console.log(error);
+                    } else {
+                        console.log(body);
+                        reply(body)
+                        // reply back with access tokens
+                    }
+                });
+        }
+    });
 
+    server.start(function (err) {
         console.log('Server started at:', server.info.uri);
     });
 /*
