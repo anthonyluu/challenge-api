2eb5d09b3d691b0c63d2b86a0ffd3d5bc7975b77
anthonyluu <anthony_luu99@hotmail.com>
2015-07-18 13:46:52 -0400

    added bell. uncomment when ready to integrate github

--- app.js
+++ app.js
@@ -1,24 +1,64 @@
-var Hapi = require('hapi');
+var Hapi = require('hapi'),
+	Bell = require('bell');
 
 var server = new Hapi.Server();
 server.connection({ port: 3000 });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
 
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
+/* // remove these comments when ready to test with github integration using Bell.
+server.register(Bell, function (err) {
 
-server.start(function () {
-    console.log('Server running at:', server.info.uri);
+    server.auth.strategy('github', 'bell', {
+        provider: 'github',
+        password: 'password',
+        isSecure: false,
+        // Make sure to set a "Callback URL" and
+        // check the "Allow this application to be used to Sign in with Twitter"
+        // on the "Settings" tab in your Twitter application
+        clientId: '',                               // Set client id
+        clientSecret: ''                            // Set client secret
+    });
+
+    server.route({
+        method: ['GET', 'POST'],
+        path: '/login',
+        config: {
+            auth: {
+                strategy: 'github',
+                mode: 'try'
+            },
+            handler: function (request, reply) {
+
+                if (!request.auth.isAuthenticated) {
+                    return reply('Authentication failed due to: ' + request.auth.error.message);
+                }
+
+                return reply.redirect('/home');
+            }
+        }
+    });
+
+*/
+	server.route({
+	    method: 'GET',
+	    path: '/',
+	    handler: function (request, reply) {
+	        reply('Hello, world!');
+	    }
+	});
+
+	server.route({
+	    method: 'GET',
+	    path: '/{name}',
+	    handler: function (request, reply) {
+	        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
+	    }
+	});
+
+    server.start(function (err) {
+
+        console.log('Server started at:', server.info.uri);
+    });
+/*
 });
+*/
