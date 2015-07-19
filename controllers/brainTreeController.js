890b9c636a5798f1a738571ecb6bf633bf924f69
p2sun <sunpeter88@gmail.com>
2015-07-19 07:46:38 -0400

    added brain tree

--- controllers/brainTreeController.js
+++ controllers/brainTreeController.js
@@ -1,7 +1,8 @@
 'use strict'
 
 var braintree = require("braintree");
-var config = require('./env.json')['brain-tree'];
+var config = require('../env.json')['brainTree'];
+
 
 var gateway = braintree.connect({
   environment: braintree.Environment.Sandbox,
@@ -9,3 +10,22 @@ var gateway = braintree.connect({
   publicKey: config.publicKey,
   privateKey: config.privateKey
 });
+console.log(gateway);
+exports.getToken = function (request, reply) {
+  gateway.clientToken.generate({}, function (err, response) {
+  	console.log(res);
+    res.send(response.clientToken);
+  });
+}
+
+exports.sendTransaction =  function (request, reply) {
+	var nonce = request.body.payment_method_nonce;
+	// Use payment method nonce here
+	gateway.transaction.sale({
+  		amount: "10.00",
+  		paymentMethodNonce: nonce,
+	}, function (err, result) {
+		console.log("purchase result: " + result.success);
+		res.send();
+	});
+}
