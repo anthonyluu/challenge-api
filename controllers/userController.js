5e1e74295546e665a249accb99c880ae293acd2c
p2sun <sunpeter88@gmail.com>
2015-07-19 02:20:34 -0400

    cleaned up user and attempt controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,45 +2,68 @@ var User = require('../models/user.models');
 
 exports.createUser = function(req,res){
     var user = new User();
-    user.Id = Math.random().toString(36).slice(2);
     user.githubID = req.body.githubID;
     user.name = req.body.name;
     user.email = req.body.email;
-    user.challenges = [];
-    user.attempts =[];
-    
+    user.active = true;
     user.save(function(error){
-        if(err) res.send(error);
+        if(err) {
+            res.send(error);
+        }
         res.json({message: 'User successfully created', data:user});
     });
 }
 
 exports.getUser = function(req, res){
-    User.find({githubID:req.params.id},function(err, user) {
-        if (err) res.send(err);
-        if(!user.active) res.send('User Does Not Exist');
-        res.json(user);
-    });
+    User.find({githubID:req.params.id},
+        function(err, user) {
+            if (err){
+                res.send(err);
+            }
+            if (!user.active){
+                res.json({message:'User Does Not Exist'});
+            }
+            res.json(user);
+        }
+    );
 }
 
 exports.getUserAttempts = function(req,res){
-    User.find({githubID:req.params.id},
-        function(err, user)  { if (err) res.send(err);})
+    User.find({githubID:req.params.githubID},
+        function(err, user)  { 
+            if (err) {
+                res.send(err);
+            }
+        }
+    )
     .populate('attempts')
     .exec(function(err, user){
-        if(err) res.send(err);
-        if(!user.active) res.send('User Does Not Exist');
+        if(err){
+            res.send(err);
+        }
+        if(!user.active){
+            res.send('User Does Not Exist');
+        }
         res.json(user.attempts);
     });
 }
 
 exports.getUserChallenges = function(req,res){
     User.find({githubID:req.params.githubID},
-        function(err, user)  { if (err) res.send(err);})
+        function(err, user)  { 
+            if (err){
+                 res.send(err);
+            }
+        }
+    )
     .populate('challenges')
     .exec(function(err, user){
-        if(err) res.send(err);
-        if(!user.active) res.send('User Does Not Exist');
+        if(err){
+            res.send(err);
+        }
+        if(!user.active){
+            res.send('User Does Not Exist');
+        }
         res.json(user.challenges);
     });
 }
@@ -60,10 +83,4 @@ exports.addChallenge = function(req, res){
     }
 }*/
 
-exports.deleteUser = function(req,res){
-    User.findByIdAndRemove(req.params.id,
-        function(err){
-            if(err) res.send(err);
-            res.json({message:'User is deleted'});
-        });
 }
